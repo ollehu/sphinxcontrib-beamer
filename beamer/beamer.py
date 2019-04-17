5eca1d41586c55ca3e07f1084ca4285056373b72
Olle Hynén Ulfsjöö <olle.hynenulfsjoo@niradynamics.se>
2019-04-17 15:50:50 +0200

    Push first working copy. TODO list at the end of beamer.py
    Change-Id: Id99af0845502a651d3a3197ffd06a7f92985c146

--- beamer/beamer.py
+++ beamer/beamer.py
@@ -1,25 +1,25 @@
-import os
+"""Sphinxextension for Beamer.
+
+Outputs a Beamer LaTeX file.
+"""
+
 from os import path
-from sphinx.locale import _,__
-from sphinx import package_dir, addnodes, highlighting
-from sphinx.util import logging
 from sphinx.builders.latex import LaTeXBuilder
 from sphinx.writers.latex import LaTeXTranslator
-from sphinx.util import logging
-from docutils.parsers.rst import Directive
-from docutils.frontend import OptionParser
-from sphinx.util.docutils import SphinxFileOutput
 from sphinx.util.template import SphinxRenderer
 
-logger = logging.getLogger(__name__)
-
-template_path = ""
+import docutils
+from docutils import nodes
 
 class BeamerTranslator(LaTeXTranslator):
+    """Beamer translator class.
+    Overloaded to define directives and render
+    the correct template.
+    """
 
     def __init__(self, document, builder):
         LaTeXTranslator.__init__(self, document, builder)
-        self.elements["maketitle"] = "\maketitle"
+        self.elements["maketitle"] = r"\maketitle"
         self.elements["makeindex"] = ""
         self.elements["printindex"] = ""
         self.elements["tableofcontents"] = ""
@@ -39,27 +39,41 @@ class BeamerTranslator(LaTeXTranslator):
         for template_dir in self.builder.config.templates_path:
             template = path.join(self.builder.confdir, template_dir,
                                  template_name)
-            template_path = template_dir
-
             if path.exists(template):
                 return BeamerRenderer(template_dir).render(template, variables)
 
         return BeamerRenderer().render(template_name, variables)
 
     def visit_title(self, node):
+        """Prepend with begin frame."""
+
         self.body.append(r"\begin{frame}{")
         self.context.append("}\n" + self.hypertarget_to(node.parent))
         self.in_title = 1
 
     def depart_title(self, node):
+        """Append with closing bracket."""
+
         self.body.append("}\n")
         self.in_title = 0
 
+    def visit_close_environment_node(self, node):
+        """Replace with end frame."""
+        self.body.append(r"\end{frame}")
+
+    def depart_close_environment_node(self, node):
+        """Do nothing."""
+        pass
+
 
 class BeamerRenderer(SphinxRenderer):
+    """Beamer renderer.
+    Overloaded to pass along the custom
+    template path.
+    """
+
     def __init__(self, template_path=""):
         # type: () -> None
-        #template_path = os.path.join(self.builder.config.templates_path)
         super(BeamerRenderer, self).__init__(template_path)
 
         # use JSP/eRuby like tagging instead because curly bracket; the default
@@ -69,14 +83,11 @@ class BeamerRenderer(SphinxRenderer):
         self.env.block_start_string = '<%'
         self.env.block_end_string = '%>'
 
-
-class BeamerDirective(Directive):
-    pass
-
-
 class BeamerBuilder(LaTeXBuilder):
     """Builder for LaTeX Beamer.
+    Overloaded to load the custom translator.
     """
+
     name = "beamer"
     format = "latex"
 
@@ -84,18 +95,35 @@ class BeamerBuilder(LaTeXBuilder):
         self.default_translator_class = BeamerTranslator
         super(BeamerBuilder, self).init()
 
+class CloseEnvironment(nodes.Admonition, nodes.Element):
+    """Empty environment.
+    Defined to be used with close_environments.
+    Closes LaTeX frame environments.
+    """
+
+    pass
+
+
+def close_environments(app, doctree, docname):
+    """Function connected to doctree-resolved.
+    Appens each section with a CloseEnvironment
+    node, which later is replaced with end frame.
+    """
+
+    for section in doctree.traverse(docutils.nodes.section):
+        node = CloseEnvironment()
+        section.append(node)
 
 def setup(app):
-    # Initialize logger.
-    logger.info("Initializing Beamer Builder")
-
+    """Setup the Sphinx extension."""
     # Register builder.
     app.add_builder(BeamerBuilder)
-    # Register directive.
-    app.add_directive("beamer", BeamerDirective)
-
-    # Add fake option to test functionality.
-    app.add_config_value("option_1", True, "env")
+    # Register close_environments node.
+    app.add_node(CloseEnvironment,
+                 beamer=(BeamerTranslator.visit_close_environment_node,
+                         BeamerTranslator.depart_close_environment_node))
+    # Connect close_environments.
+    app.connect("doctree-resolved", close_environments)
 
     return {
         "version": "0.1",
@@ -103,20 +131,12 @@ def setup(app):
         "parallel_write_safe": True,
     }
 
-""" This needs to be done for 'make beamer' to compile:
-        [x] Insert
-            \newcommand{\paragraph}{}
-            \newcommand{\subparagraph}{}
-          in the pre-ample. For titlesec compability.
-        [x] Replace sphinxmaketitle with maketitle for footnote
-          compability.
-        [x] Replace the documentclass to beamer.
-        [ ] Replace \chapter{#1} with
-            \begin{frame}{#1}
-                ...
-            \end{frame}
-        [x] Remove 'Indices and tables' all together.
-        [x] Don't print indices.
-        [x] Don't print table of contents.
-        [x] Don't print \pagestyle{normal}.
+"""
+    TODO
+        * Replace subsections with bold, or something
+          subsection-looking in beamer. Not with a new frame.
+        * Use the NIRA Beamer template.
+
+       (* Solve the highlightning warning?)
+       (* Solve the PyLint warnings?)
 """
